64febf9f92c8520c14b97a04a5220d1074b67cde
Can Ngo <54816096+Cngo2019@users.noreply.github.com>
2022-10-17 14:47:43 -0400

    Add files via upload

---
+++ PICKUP_DATABASE_CREATION.sql
@@ -0,0 +1,105 @@
+# DROP SCHEMA pickup;  # uncomment this line if you get an "database already exists" error or something along those lines
+CREATE SCHEMA pickup;
+USE pickup;
+
+CREATE TABLE sports(
+	sport_id int NOT NULL AUTO_INCREMENT,
+    sport_name varchar(255) NOT NULL,
+    PRIMARY KEY(sport_id)
+);
+
+CREATE TABLE accounts(
+	account_id int NOT NULL AUTO_INCREMENT,
+    first_name varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    account_username varchar(255) NOT NULL, 
+    account_password varchar(255) NOT NULL,
+    games_joined int NOT NULL,
+    games_attended int NOT NULL,
+    rating int NOT NULL,
+    PRIMARY KEY(account_id)
+);
+
+CREATE TABLE pickup_events(
+	event_id int NOT NULL AUTO_INCREMENT,
+    account_id int NOT NULL,
+    sport_id int NOT NULL,
+    maximum_players int NOT NULL,
+    current_players int NOT NULL,
+    event_location varchar(255),
+    event_date varchar(255),
+    event_time varchar(255),
+    PRIMARY KEY (event_id),
+    FOREIGN KEY (account_id) REFERENCES accounts(account_id) ON DELETE CASCADE,
+    FOREIGN KEY (sport_id) REFERENCES sports(sport_id) ON DELETE CASCADE
+);
+
+# JUNCTION TABLES #
+
+# If we query by the player's ID then it will give us all events the person is attending.
+# If we query by the event ID it will give us all the players associated with that event
+CREATE TABLE player_event(
+	account_id INT NOT NULL,
+    event_id INT NOT NULL,
+    PRIMARY KEY (account_id, event_id),
+    FOREIGN KEY (account_id) REFERENCES accounts(account_id) ON DELETE CASCADE,
+    FOREIGN KEY (event_id) REFERENCES pickup_events(event_id) ON DELETE CASCADE
+);
+
+# This table will allow us to list out this player's favorite sports
+CREATE TABLE player_sport_favorite(
+	account_id INT NOT NULL,
+    sport_id INT NOT NULL,
+    PRIMARY KEY (account_id, sport_id),
+    FOREIGN KEY (account_id) REFERENCES accounts(account_id) ON DELETE CASCADE,
+    FOREIGN KEY (sport_id) REFERENCES sports(sport_id) ON DELETE CASCADE
+);
+
+# This table should be a two way releationship. I read online somewhere that it was the case.
+CREATE TABLE player_friend(
+	account_id INT NOT NULL,
+    friend_id INT NOT NULL,
+	FOREIGN KEY (account_id) REFERENCES accounts(account_id) ON DELETE CASCADE,
+    FOREIGN KEY (friend_id) REFERENCES accounts(account_id) ON DELETE CASCADE
+);
+
+INSERT INTO sports(sport_name)
+VALUES ('Soccer'), ('Football'), ('Basketball');
+
+INSERT INTO accounts(first_name, last_name, email, account_username, account_password, games_joined, games_attended, rating)
+VALUES 
+	('Thomas', 'Zbdoula', 'email', 'tz', '123', 10, 10, 0),
+    ('john', 'jr', 'email', 'god_of_balls', 'abc', 5, 5, 0),
+    ('aaa', 'aaa', 'email' ,'aaa', 'aaa', 9, 100, 0);
+
+INSERT INTO pickup_events(account_id, sport_id, maximum_players, current_players, event_location, event_date, event_time)
+VALUES 
+    (1, 1, 6, 4, "urec nc charlotte", "10/13/2022", "6:30 PM"),
+	(1, 1, 6, 4, "urec nc charlotte", "10/13/2022", "6:30 PM"),
+	(3, 2, 6, 4, "urec nc charlotte", "10/13/2022", "6:30 PM"),
+	(2, 2, 6, 4, "urec nc charlotte", "10/13/2022", "6:30 PM");
+    
+INSERT INTO player_event(account_id, event_id)
+VALUES 
+	(1, 1),
+    (1, 2),
+    (2, 1),
+    (3, 1),
+    (3, 3),
+    (2, 4)
+;
+    
+INSERT INTO player_sport_favorite(account_id, sport_id)
+VALUES
+    (1, 1),
+    (1, 3),
+    (2, 1),
+    (3, 3)
+;
+
+SELECT * FROM pickup_event;
+DELETE FROM pickup_events
+WHERE event_id=1;
+
+SELECT * FROM  player_event;
